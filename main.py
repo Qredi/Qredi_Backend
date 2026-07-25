d27303331b07240d68d4e03c2296999db3869827
Myakey <goldenation139@gmail.com>
2026-07-25 15:22:26 +0700

    Feat: Adding Cors Origin

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 
 from app.api.v1 import all_routers
 
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,6 +15,14 @@ app = FastAPI(
     title="Qredi Backend API",
     version="0.1.0",
     lifespan=lifespan,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 for router in all_routers:
